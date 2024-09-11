6cc9fb0aeef4cc7144f2e191fd50d97e50a953e2
Aya <belaroussiay14@gmail.com>
2024-09-11 16:53:17 +0100

    changes files

--- script/script.js
+++ script/script.js
@@ -1,7 +1,19 @@
-function test ( ){
-  Swal.fire({
-      title: "Good job!",
-      text: "Your registration has been completed!",
-      icon: "success"
-    });
+function sendMail(){
+  var params= {
+  FirstName:document.getElementById("name").value,
+  email:document.getElementById("email").value,
+  password:document.getElementById("password").value,
+  };
+  const serviceID ="service_iysezge"
+  const templateID ="template_wvldnae"
+ email.js(serviceID,templateID,params)
+.then(
+  res =>{
+    document.getElementById("name").value ="";
+    document.getElementById("email").value="";
+    document.getElementById("password").value="";
+    alert("Your registration is validated.")
+
+    }) 
+    .catch((err)=>console.log(err));
 }
